3779abf26dc47a8c47cc1a8649bf04af79e348ce
Pieter-1337 <pieterbracke@msn.com>
2018-05-31 15:28:24 +0200

    Added IActionResult Get (QuizController) for getting one Quiz

--- TestMakerFree/TestMakerFreeWebApp/Controllers/QuizController.cs
+++ TestMakerFree/TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -5,12 +5,49 @@ using TestMakerFreeWebApp.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace TestMakerFreeWebApp.Controllers
 {
     [Route("api/[controller]")]
     public class QuizController : Controller
     {
-        // GET api/quiz/latest
+        #region RESTful Convention methods
+        /// <summary>
+        /// GET: api/quiz/{}id
+        /// Retrieves the Quiz with the given {id}
+        /// </summary>
+        /// <param name="id">The ID of an existing Quiz</param>
+        /// <returns>the Quiz with the given {id}</returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //Create a sample Quiz to match the given request
+            var v = new QuizViewModel()
+            {
+                Id = id,
+                Title = String.Format($"Sample quiz with id {id}"),
+                Description = "Not a real quiz: it's just a sample",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            };
+
+            //Output the result in a Json Format
+            return new JsonResult(v, 
+                new JsonSerializerSettings
+                    {
+                        Formatting = Formatting.Indented
+                    });
+        }
+        #endregion
+
+
+        #region Attribute-based Routing methods
+        /// <summary>
+        /// GET: api/quiz/latest
+        /// Retrieves the {num} latest Quizzes
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>the {num} latest Quizzes</returns>
         [HttpGet("Latest/{num}")]
         public IActionResult Latest(int num = 10)
         {
@@ -43,6 +80,7 @@ namespace TestMakerFreeWebApp.Controllers
                     Formatting = Formatting.Indented
                 });
         }
+        #endregion
 
         /// <summary>
         /// GET: api/quiz/ByTitle
